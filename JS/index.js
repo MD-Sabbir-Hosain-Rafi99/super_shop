c08b8a4f6c91fa34ec00476ded821680420cb1ac
MD-Sabbir-Hosain-Rafi99 <sabbirhossain.rafi121@gmail.com>
2025-06-01 11:46:17 +0600

    first commit

---
+++ JS/index.js
@@ -0,0 +1,51 @@
+let sum = 0;
+let totalPrice = document.getElementById('totalPrice');
+function buyNow(target) {
+  // ======== Product Name Dhora Hoice ======== 
+  let selectedItem = document.getElementById('selectedItem');
+  let productName = target.parentNode.childNodes[3].innerText;
+  let li = document.createElement('li');
+  li.textContent = productName;
+  selectedItem.appendChild(li)
+  // ======== Product Name Dhora Hoice ========
+
+  // ======== Product Price Dhora Hoice ========
+  // console.log(target.parentNode.childNodes[5].childNodes[1].innerText);
+  let productPrice = parseFloat(target.parentNode.childNodes[5].childNodes[1].innerText);
+  sum += productPrice
+  totalPrice.innerHTML = sum;
+  // ======== Product Price Dhora Hoice ========
+}
+
+
+let couponCode = document.getElementById('couponCode');
+let discountPrice = document.getElementById('discountPrice');
+let finalPrice = document.getElementById('finalPrice');
+function applyCupon(){
+  if (couponCode.value == "SELL200") {
+    let discountPriceValue = (20/100) * sum;
+    let finalDiscountPrice = sum - discountPriceValue;
+    discountPrice.innerHTML = discountPriceValue;
+    finalPrice.innerHTML = finalDiscountPrice;
+  }else if (couponCode.value == "") {
+    alert("Please Enter Cupon Code!")
+  }else{
+    alert("This is not right discount code!")
+  }
+}
+
+
+
+// function makePurchase(){
+//   if (sum == sum && ) {
+//     alert(`Dhonnobad total ${sum} taka product kinar jonno without discount!`)
+    
+//   }else if (discountPrice.innerHTML < finalPrice.innerHTML) {
+//     alert(`Apnar Total Price ${sum} Taka, After 20% Discount ${discountPrice.innerHTML} Taka Peyechen, After Discount Total ${finalPrice.innerHTML} Taka`)
+//   }
+// }
+
+
+
+
+
